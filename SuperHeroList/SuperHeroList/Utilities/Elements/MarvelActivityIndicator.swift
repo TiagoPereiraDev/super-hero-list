fea9ed3c81703e2714a02f0c55ea84de5088c012
Tiago Pereira <highbugs@gmail.com>
2020-10-04 15:46:38 +0200

    Add tests to activity indicator

--- SuperHeroList/SuperHeroList/Utilities/Elements/MarvelActivityIndicator.swift
+++ SuperHeroList/SuperHeroList/Utilities/Elements/MarvelActivityIndicator.swift
@@ -9,12 +9,21 @@
 import UIKit
 import RxSwift
 
+/// Class that will handle all the activity indicator in the app
+/// basically it can register an observer to listen when is loading or not
+/// and it will show or not the activity indicator in a container passed by constructor
 class MarvelActivityIndicator {
-        
+    
     private let disposeBag = DisposeBag()
     private weak var container: UIView?
     private let activityIndicator: UIActivityIndicatorView
+    private let didChangeState = PublishSubject<Bool>()
     
+    /**
+    Class constructor
+    
+    - Parameter container: view where we want the activity indicator to appear
+    */
     init(container: UIView) {
         self.container = container
         self.activityIndicator = UIActivityIndicatorView(style: .whiteLarge)
@@ -22,7 +31,15 @@ class MarvelActivityIndicator {
         self.activityIndicator.translatesAutoresizingMaskIntoConstraints = false
     }
     
-    func registerLoading(loading: Observable<Bool>) {
+    /**
+     Register the loading observer to know when to show / hidde the activity indicator
+     
+     - Parameter loading: loading observer responsable for the process that's going on
+     
+     - Returns: Observable to notify when loading changes were aplied indicating if is loading or not
+     */
+    @discardableResult
+    func registerLoading(loading: Observable<Bool>) -> Observable<Bool> {
         loading.subscribe(onNext: { loading in
             DispatchQueue.main.async {
                 if loading {
@@ -30,15 +47,22 @@ class MarvelActivityIndicator {
                 } else {
                     self.removeActivityIndicator()
                 }
+                self.didChangeState.onNext(loading)
             }
         }).disposed(by: self.disposeBag)
+        
+        return self.didChangeState
     }
     
-    func addActivityIndicator() {
+    /**
+        Method that will insert the activity indicator in the container and start animating,
+     it sets all the constraints as well
+        */
+    private func addActivityIndicator() {
         if let container = self.container {
             self.activityIndicator.startAnimating()
             container.addSubview(self.activityIndicator)
-         
+            
             NSLayoutConstraint.activate([
                 activityIndicator.centerYAnchor.constraint(equalTo: container.centerYAnchor),
                 activityIndicator.centerXAnchor.constraint(equalTo: container.centerXAnchor)
@@ -46,8 +70,33 @@ class MarvelActivityIndicator {
         }
     }
     
-    func removeActivityIndicator() {
+    /**
+     Method that will remove activity indicator from the container and stop animating
+     */
+    private func removeActivityIndicator() {
         self.activityIndicator.stopAnimating()
         self.activityIndicator.removeFromSuperview()
+    }
+    
+    /**
+     Method to check if activity indicator is animating or not
+     
+     - Returns: Bool value indicating if activity indicator is animating or not
+     */
+    public func isActivityIndicatorAnimating() -> Bool {
+        return self.activityIndicator.isAnimating
+    }
+    
+    /**
+     Method to check if activity indicator is in the container or not
+     
+     - Returns: Bool value indicating if activity indicator is in the container or not
+     */
+    public func isActivityInContainer() -> Bool {
+        guard let container = self.container else {
+            return false
+        }
+        
+        return container.subviews.contains(self.activityIndicator)
     }
 }
---
+++ SuperHeroList/SuperHeroListTests/MarvelActivityIndicatorTest.swift
@@ -0,0 +1,65 @@
+//
+//  MarvelActivityIndicatorTest.swift
+//  SuperHeroListTests
+//
+//  Created by Tiago Pereira on 04/10/2020.
+//  Copyright © 2020 Tiago Pereira. All rights reserved.
+//
+
+import XCTest
+import RxSwift
+import RxTest
+
+class MarvelActivityIndicatorTest: XCTestCase {
+    var scheduler: TestScheduler!
+    var disposeBag: DisposeBag!
+    var container: UIView!
+    
+    override func setUp() {
+        container = UIView()
+        disposeBag = DisposeBag()
+        scheduler = TestScheduler(initialClock: 0)
+    }
+    
+    func testLoading() {
+        let expectation = XCTestExpectation(description: "Waiting for loading")
+        let activityIndicator = MarvelActivityIndicator(container: container)
+        
+        let loading = PublishSubject<Bool>()
+        
+        activityIndicator.registerLoading( loading: loading).subscribe(onNext: { _ in
+            XCTAssertTrue(activityIndicator.isActivityInContainer())
+            XCTAssertTrue(activityIndicator.isActivityIndicatorAnimating())
+            expectation.fulfill()
+        }).disposed(by: self.disposeBag)
+        
+        loading.onNext(true)
+        
+       wait(for: [expectation], timeout: 1)
+    }
+    
+    func testNotLoading() {
+        let expectation = XCTestExpectation(description: "Waiting for loading")
+        let activityIndicator = MarvelActivityIndicator(container: container)
+         
+        let loading = PublishSubject<Bool>()
+        
+        XCTAssertFalse(activityIndicator.isActivityInContainer())
+        XCTAssertFalse(activityIndicator.isActivityIndicatorAnimating())
+        
+        activityIndicator.registerLoading( loading: loading)
+            .subscribe(onNext: { isLoading in
+                if !isLoading {
+                    XCTAssertFalse(activityIndicator.isActivityInContainer())
+                    XCTAssertFalse(activityIndicator.isActivityIndicatorAnimating())
+                    expectation.fulfill()
+                }
+            }).disposed(by: self.disposeBag)
+        
+        loading.onNext(true)
+        loading.onNext(false)
+         
+        wait(for: [expectation], timeout: 1)
+    }
+
+}
